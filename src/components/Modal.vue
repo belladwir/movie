a54e9e1115b72b9288cadb69af43a8e9d2dcb1de
belladwir <belladwirh@gmail.com>
2022-01-06 17:35:52 +0700

    show modal poster

---
+++ src/components/Modal.vue
@@ -0,0 +1,84 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal-lg" role="dialog">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            @click="close"
+          ></button>
+        </div>
+        <section
+          class="modal-body d-flex justify-content-center m-2"
+          id="modalDescription"
+        >
+          <img :src="poster" />
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["poster"],
+  methods: {
+    close() {
+      this.$parent.closeModal();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(136, 136, 136, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-lg {
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+.modal-header {
+  position: relative;
+  border-bottom: 1px solid #eeeeee;
+  color: #000000;
+  justify-content: space-between;
+}
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  flex-direction: column;
+}
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,51 @@
 <template>
   <div id="app">
-    <h3>{{ msg }}</h3>
+    <p class="text-start fw-bold m-3">{{ msg }}</p>
     <div class="row">
       <div class="col-sm-4" v-for="movie in movies" :key="movie.imdbID">
         <div class="card m-3 rounded-3">
-          <img :src="movie.Poster" class="card-img-top" alt="..." />
+          <img
+            :src="movie.Poster"
+            class="card-img-top"
+            :alt="movie.Title"
+            style="cursor: pointer"
+            @click="showModal(movie.Poster)"
+          />
           <div class="card-body">
-            <h5 class="card-title">{{ movie.Title }}</h5>
+            <p class="text-start card-title fw-bold">{{ movie.Title }}</p>
+            <p class="text-start fs-6">{{ movie.Year }}</p>
           </div>
         </div>
       </div>
+      <Modal v-show="isModalVisible" v-bind:poster="poster" />
     </div>
   </div>
 </template>
 
 <script>
+import Modal from "@/components/Modal.vue";
+
 export default {
   name: "Home",
+  components: {
+    Modal,
+  },
   data() {
     return {
       msg: "Movies List From OMDB API",
+      isModalVisible: false,
+      poster: null,
     };
+  },
+  methods: {
+    showModal(poster) {
+      this.isModalVisible = true;
+      this.poster = poster;
+      console.log(this.poster);
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
   },
   computed: {
     movies() {
