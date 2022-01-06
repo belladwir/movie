330803d2f6a163372c0a5eba574a00b06cbedcef
belladwir <belladwirh@gmail.com>
2022-01-06 16:00:46 +0700

    Integrate with API OMDB

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,41 @@
-import { createStore } from "vuex";
+// import { createStore } from "vuex";
+import axios from "axios";
+import Vuex from "vuex";
+// import Vue from "vue";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+//load Vuex
+// Vue.use(Vuex);
+
+//to handle state
+const state = {
+  movies: [],
+};
+
+//to handle state
+const getters = {};
+
+//to handle actions
+const actions = {
+  getMovies({ commit }) {
+    axios
+      .get("https://www.omdbapi.com/?apikey=715289b&s=Batman&page=1")
+      .then((response) => {
+        commit("SET_MOVIES", response.data["Search"]);
+      });
+  },
+};
+
+//to handle mutations
+const mutations = {
+  SET_MOVIES(state, movies) {
+    state.movies = movies;
+  },
+};
+
+//export store module
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <h3>{{ msg }}</h3>
+    <div class="row">
+      <div class="col-sm-4" v-for="movie in movies" :key="movie.imdbID">
+        <div class="card m-3 rounded-3">
+          <img :src="movie.Poster" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ movie.Title }}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      msg: "Movies List From OMDB API",
+    };
+  },
+  computed: {
+    movies() {
+      return this.$store.state.movies;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getMovies");
   },
 };
 </script>
